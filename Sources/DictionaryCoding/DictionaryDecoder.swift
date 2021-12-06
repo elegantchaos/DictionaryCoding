ac75dbbc2cc7261e2d1874c4a76e4d1b50ad9b26
Robert Gering <mail@robertgering.de>
2021-12-06 14:48:29 +0100

    Return Data directly if present

--- Sources/DictionaryCoding/DictionaryDecoder.swift
+++ Sources/DictionaryCoding/DictionaryDecoder.swift
@@ -1288,6 +1288,10 @@ extension _DictionaryDecoder {
     fileprivate func unbox(_ value: Any, as type: Data.Type) throws -> Data? {
         guard !(value is NSNull) else { return nil }
         
+        if let data = value as? Data {
+            return data
+        }
+
         switch self.options.dataDecodingStrategy {
         case .deferredToData:
             self.storage.push(container: value)
